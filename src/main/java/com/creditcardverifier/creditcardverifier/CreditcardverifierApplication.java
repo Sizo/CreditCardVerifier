7fac7902f03a28113cf27711aa2e46b60fe289fb
Sizo Duma <sizo.duma@gmail.com>
2023-02-13 18:52:46 +0200

    project creation

---
+++ src/main/java/com/creditcardverifier/creditcardverifier/CreditcardverifierApplication.java
@@ -0,0 +1,13 @@
+package com.creditcardverifier.creditcardverifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditcardverifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditcardverifierApplication.class, args);
+	}
+
+}
